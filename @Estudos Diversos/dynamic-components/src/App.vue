7eff0d70cf66ba86a3b2fdd12e0a5c9584635ed4
Rafaelmmt <144057595+Rafaelmmt@users.noreply.github.com>
2024-10-08 14:53:48 -0300

    Proj02

---
+++ @Estudos Diversos/dynamic-components/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <select v-model="componentName">
+    <option value="Home">Home</option>
+    <option value="About">About</option>
+  </select>
+  <br>
+  <keep-alive>
+    <component :is="components[componentName]"></component>
+  </keep-alive>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+
+const components = {
+  Home,
+  About,
+}
+
+const componentName = ref('Home')
+</script>
---
+++ @Estudos Diversos/dynamic-components/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Home</h1>
+  <h6>Component home ativo!</h6>
+</template>
+
+<script setup>
+import { onMounted, onUnmounted, onActivated, onDeactivated } from 'vue'
+
+onMounted(() => {
+  console.log(`the component HOME is now mounted.`)
+})
+
+onUnmounted(() => {
+  console.log(`the component HOME was unmounted.`)
+})
+
+onActivated(() => {
+  console.log(`the component HOME ATIVADO.`)
+})
+
+onDeactivated(() => {
+  console.log(`the component HOME DESATIVADO.`)
+})
+</script>
